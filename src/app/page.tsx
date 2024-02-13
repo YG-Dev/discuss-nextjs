f1ff2c51cbafd93bd3e92cd5b637c328342473ab
YG-Dev <masiejczyk.dawid@gmail.com>
2024-02-13 16:51:31 +0100

    feat/Add route pathing base and session

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,25 @@
 import { Button } from '@nextui-org/react';
+import * as actions from '@/actions';
+import { auth } from '@/auth';
+import Profile from '@/components/profile';
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth();
+  
   return (
     <div>
-      <Button>Click</Button>
+      <form action={actions.signIn}>
+        <Button type="submit">Sign in</Button>
+      </form>
+
+      <form action={actions.signOut}>
+        <Button type="submit">Sign out</Button>
+      </form>
+      {
+        session?.user ? <div>Signed in</div> : <div>Signed out</div>
+      }
+
+      <Profile />
     </div>
   );
 }
