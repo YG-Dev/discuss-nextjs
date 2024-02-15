18445946d679672ca30b179d0fa4fc4d5896184e
YG-Dev <masiejczyk.dawid@gmail.com>
2024-02-15 12:55:39 +0100

    feat/add header auth for static homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,7 @@
-import { Button } from '@nextui-org/react';
-import * as actions from '@/actions';
-import { auth } from '@/auth';
-import Profile from '@/components/profile';
-
-export default async function Home() {
-  const session = await auth();
-  
+export default function Home() {
   return (
     <div>
-      <form action={actions.signIn}>
-        <Button type="submit">Sign in</Button>
-      </form>
-
-      <form action={actions.signOut}>
-        <Button type="submit">Sign out</Button>
-      </form>
-      {
-        session?.user ? <div>Signed in</div> : <div>Signed out</div>
-      }
-
-      <Profile />
+      Home Page
     </div>
   );
 }
