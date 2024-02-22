f7a85d55bff41f5ba115128128b2578f06b350e2
YG-Dev <masiejczyk.dawid@gmail.com>
2024-02-22 12:40:59 +0100

    feat/add more updated requests

--- src/app/topics/[slug]/page.tsx
+++ src/app/topics/[slug]/page.tsx
@@ -1,10 +1,12 @@
 import PostCreateForm from "@/components/posts/post-create-form";
+import PostList from "@/components/posts/post-list";
+import { fetchPostsByTopicSlug } from "@/db/queries/post";
 
 interface TopicShowPageProps {
     params: {
         slug: string;
     }
-}
+};
 
 export default function TopicShowPage({ params }: TopicShowPageProps) {
     const { slug } = params;
@@ -15,6 +17,7 @@ export default function TopicShowPage({ params }: TopicShowPageProps) {
                 <h1 className="text-2xl font-bold mb-2">
                     {slug}
                 </h1>
+                <PostList fetchData={() => fetchPostsByTopicSlug(slug)} />
             </div>
             <div>
                 <PostCreateForm slug={slug} />
